ea1e5312e2d20ced3b7fac04979bc7d06a164e10
pkpotter03 <pkpotter03@gmail.com>
2025-09-27 23:25:31 +0530

    fixing code

--- controllers/authController.js
+++ controllers/authController.js
@@ -85,7 +85,7 @@ export const register = async (req, res) => {
 export const login = async (req, res) => {
   const { email, password } = req.body;
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email }).select("-password");
     if (user && (await bcrypt.compare(password, user.password))) {
 
       const accessToken = generateAccessToken(user._id);
