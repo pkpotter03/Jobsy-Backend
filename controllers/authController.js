f6281f6046abb9a09af8a7afaab9ed7e6dd64619
pkpotter03 <pkpotter03@gmail.com>
2025-09-28 16:51:38 +0530

    add cookies adjustment

--- controllers/authController.js
+++ controllers/authController.js
@@ -13,19 +13,21 @@ const generateRefreshToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_REFRESH_SECRET, { expiresIn: "7d" });
 }
 
-const setTokensInCookies = (res, accessToken, refreshToken) => {
+export const setTokensInCookies = (res, accessToken, refreshToken) => {
+  const isProd = process.env.NODE_ENV === "production";
+
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 15 * 60 * 1000
+    secure: isProd,       
+    sameSite: isProd ? "none" : "lax", 
+    maxAge: 15 * 60 * 1000, 
   });
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
-    secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
-    maxAge: 7 * 24 * 60 * 60 * 1000
+    secure: isProd,
+    sameSite: isProd ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
   });
 };
 
